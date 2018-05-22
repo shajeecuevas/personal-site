90dd1cb106377179575f1af8a17f354b5cbeacea
Shajee <shajeecuevas@gmail.com>
2018-05-22 14:22:30 -0400

    Fixed nav bar and added more to the projects section

--- src/app/header.component.ts
+++ src/app/header.component.ts
@@ -3,21 +3,29 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'header-component',
     template: `
-    <div id="margin-top">
-            <ul class="nav nav-pills navbar-fixed-top nav-background">
-                <a> Shajee Cuevas </a>
-                <ul class="nav  nav-pills navbar-right">
-                    <li role="presentation" class="active">
-                        <a href="#">About Me</a>
-                    </li>
-                    <li role="presentation">
-                        <a href="#">Projects</a>
-                    </li>
-                    <li role="presentation">
-                        <a href="#">Blog</a>
-                    </li>
-                </ul>
-            </ul>
+    <div class="nav-background nav nav-pills navbar-fixed-top">
+        <div class="container">
+            <div class="navbar-header page-scroll">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span> Menu
+                </button>
+                <a class="navbar-brand" href="#page-top">Shajee Cuevas</a>
+            </div>
+
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                    <ul class="nav  nav-pills navbar-right">
+                        <li role="presentation" class="page-scroll">
+                            <a href="#about">About Me</a>
+                        </li>
+                        <li role="presentation" class="page-scroll">
+                            <a href="#projects">Projects</a>
+                        </li>
+                        <li role="presentation" class="page-scroll">
+                            <a href="#blog">Blog</a>
+                        </li>
+                    </ul>
+            </div>
+        </div>
     </div>
     `,
     styleUrls: ['./header.component.less']
--- src/app/projects.component.ts
+++ src/app/projects.component.ts
@@ -3,8 +3,30 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'projects-component',
     template: `
-    <div class="col-lg-12 text-center">
-        <h2> Projects </h2>
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12 text-center">
+                <h2> Projects </h2>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-sm-4 portfolio-item">
+                <div class="caption">
+                    <div class="caption-content">
+                    </div>
+                </div>
+                <img src="/../../img/tek7.png" class="img-fluid" alt="Responsive image">
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-sm-4 portfolio-item">
+                <div class="caption">
+                    <div class="caption-content">
+                    </div>
+                </div>
+                <img src="/../img/SFVlogo.png" alt="Street Fighter Frame App">
+            </div>
+        </div>
     </div>
     `,
     styleUrls: ['./projects.component.less']
